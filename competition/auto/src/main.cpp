bf797ef75ef23447a491782e047a3e604934dde4
Garfieldcmix <chayanon.nin@gmail.com>
2025-10-30 14:52:29 +0700

    feat: Add ultrasonic to auto robot code

--- competition/auto/src/main.cpp
+++ competition/auto/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 
+#include <HCSR04.h>
 #include <Servo.h>
 
 #include "gyro_tracker.h"
@@ -9,6 +10,10 @@ constexpr byte BUTTON_PIN = 10;
 constexpr byte SERVO_PIN = A0;
 constexpr double SERVO_MIN = 20;
 constexpr double SERVO_MAX = 140;
+
+const byte TRIGGER_PIN = 11;
+const byte ECHO_COUNT = 1;
+byte *ECHO_PINS = new byte[ECHO_COUNT]{12};
 
 GyroTracker tracker;
 
@@ -25,6 +30,7 @@ void setup() {
     Serial.begin(115200);
 
     armServo.attach(SERVO_PIN);
+    HCSR04.begin(TRIGGER_PIN, ECHO_PINS, ECHO_COUNT);
 
     pinMode(BUTTON_PIN, INPUT_PULLUP);
     delay(100);
@@ -62,6 +68,18 @@ void loop() {
     Serial.println(currentYaw, 2);
 
     armServo.write(SERVO_MIN);
+
+    // double *distances = HCSR04.measureDistanceCm();
+
+    // for (int i = 0; i < ECHO_COUNT; i++) {
+    //     if (i > 0) Serial.print(" | ");
+    //     Serial.print(i + 1);
+    //     Serial.print(": ");
+    //     Serial.print(distances[i]);
+    //     Serial.print(" cm");
+    // }
+
+    // Serial.println("");
 
     delay(5);
 }
