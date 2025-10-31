e043354d6ae6031f5444ff2320c0c906b9614e56
Garfieldcmix <chayanon.nin@gmail.com>
2025-10-31 23:25:33 +0700

    fix: Fix the pinfor motor

--- competition/auto_test_motor/src/main.cpp
+++ competition/auto_test_motor/src/main.cpp
@@ -2,7 +2,7 @@
 
 #define MOTOR_LEFT_1 7
 #define MOTOR_LEFT_2 8
-#define MOTOR_LEFT_EN 9
+#define MOTOR_LEFT_EN 6
 
 #define MOTOR_RIGHT_1 4
 #define MOTOR_RIGHT_2 5
@@ -29,13 +29,33 @@ void loop() {
 
     delay(2000);
 
+    digitalWrite(MOTOR_LEFT_1, LOW);
+    digitalWrite(MOTOR_LEFT_2, HIGH);
+    analogWrite(MOTOR_LEFT_EN, 255);
+
+    digitalWrite(MOTOR_RIGHT_1, LOW);
+    digitalWrite(MOTOR_RIGHT_2, HIGH);
+    analogWrite(MOTOR_RIGHT_EN, 255);
+
+    delay(2000);
+
     digitalWrite(MOTOR_LEFT_1, HIGH);
     digitalWrite(MOTOR_LEFT_2, LOW);
-    analogWrite(MOTOR_LEFT_EN, 0);
+    analogWrite(MOTOR_LEFT_EN, 255);
+
+    digitalWrite(MOTOR_RIGHT_1, LOW);
+    digitalWrite(MOTOR_RIGHT_2, HIGH);
+    analogWrite(MOTOR_RIGHT_EN, 255);
+
+    delay(2000);
+
+    digitalWrite(MOTOR_LEFT_1, LOW);
+    digitalWrite(MOTOR_LEFT_2, HIGH);
+    analogWrite(MOTOR_LEFT_EN, 255);
 
     digitalWrite(MOTOR_RIGHT_1, HIGH);
     digitalWrite(MOTOR_RIGHT_2, LOW);
-    analogWrite(MOTOR_RIGHT_EN, 0);
+    analogWrite(MOTOR_RIGHT_EN, 255);
 
     delay(2000);
 }
